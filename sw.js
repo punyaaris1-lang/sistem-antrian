ebcb6ddc1292ed1290f63cfecaa012a4ffee98ef
punyaaris1-lang <punyaaris1@gmail.com>
2025-12-04 07:09:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
 const CACHE_NAME = 'mlu-app-v1';
 const FILES_TO_CACHE = [
+  './',
   'index.html',
   'lokasi.html',
   'sos.html',
   'antrian.html',
-  '1763947427555.jpg'
+  '1763947427555.jpg',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700;900&family=Rajdhani:wght@500;600;700&family=Inter:wght@400;600;800&display=swap'
 ];
 
+// Install Service Worker
 self.addEventListener('install', (evt) => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -15,6 +20,7 @@ self.addEventListener('install', (evt) => {
   );
 });
 
+// Fetch Data (Offline Mode)
 self.addEventListener('fetch', (evt) => {
   evt.respondWith(
     caches.match(evt.request).then((res) => {
